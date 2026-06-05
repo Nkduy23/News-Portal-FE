a9ebe343f88bff7a9fab3d35f9d2b6d047a586e3
Nkduy23 <nguyenkhanhduy23803@gmail.com>
2026-06-05 13:47:30 +0700

    fix: next config

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  images: { domains: ["picsum.photos"] },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
